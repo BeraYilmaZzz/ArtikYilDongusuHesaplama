cd65c494532f091dcb11db820063b6e5a4e6d26b
BeraYilmaz <berayilmazbm@gmail.com>
2023-09-15 09:25:48 +0300

    ArtikYilDongusu

--- Main.java
+++ Main.java
@@ -5,10 +5,16 @@ public class Main {
         Scanner inp = new Scanner(System.in);
         System.out.print("Yıl Giriniz :");
         yıl = inp.nextInt() ;
-        if(yıl%4 == 0 || yıl%400 == 0){
-            System.out.println( yıl + "bir artık yıldır !");
-        }else {
-            System.out.println(yıl + "bir artık yıl değildir !! ");
+        if ( yıl%100 == 0){
+            if (yıl%400 == 0){
+                System.out.println( yıl + " bir artık yıldır !");
+            }else {
+                System.out.println(yıl + " bir artık yıl değildir !! ");
+            }
+        }else if (yıl%4 == 0){
+            System.out.println( yıl + " bir artık yıldır !");
+        } else {
+            System.out.println(yıl + " bir artık yıl değildir !! ");
         }
     }
 }
